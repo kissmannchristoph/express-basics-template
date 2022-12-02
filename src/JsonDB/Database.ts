52f618c62362bd116b07eaa33306b96d0e7ce6cd
christoph kissmann <kissmannchristoph@gmail.com>
2022-12-02 03:29:57 +0000

    ch

--- src/JsonDB/Database.ts
+++ src/JsonDB/Database.ts
@@ -1,4 +1,5 @@
 import fs, { readFileSync } from "fs";
+import DynamicClass, { DynamicClassLoadError } from "../class/DynamicClass";
 
 interface tableI {
   schema: schema_entry[];
@@ -7,6 +8,7 @@ interface tableI {
 }
 
 interface schema_entry {
+  name: string;
   key?: string;
   plugins?: string[];
 }
@@ -19,10 +21,7 @@ interface key_entry {
 class Keys {
   private keys: key_entry[];
 
-  constructor(
-    public readonly json: key_entry[],
-    private readonly schema: Schema
-  ) {}
+  constructor(public json: key_entry[], private readonly schema: Schema) {}
 
   getPrimary(): key_entry {
     //check prim key exists
@@ -37,18 +36,37 @@ class Keys {
   }
 }
 class Schema {
-  constructor(public readonly json: any) {}
+  constructor(public json: any) {}
+
+  public getByName(name: string): schema_entry {
+    return this.json.find((entry: schema_entry) => entry.name === name);
+  }
+
+  public get(): schema_entry[] {
+    return this.json;
+  }
+
+  public getIndexes(): string[] {
+    let indexes = [];
+
+    for (let entry of this.get()) {
+      indexes.push(entry.name);
+    }
+
+    return indexes;
+  }
 }
 class Data {
-  constructor(public readonly json: {}[]) {}
+  constructor(public json: any[]) {}
 }
 
 class Query {
   private operations: string[] = [];
 
   constructor(
-    private readonly table: table,
-    private readonly operation: "select"
+    private table: table,
+    private readonly operation: "select",
+    private readonly select: string[]
   ) {}
 
   private _orderBy: any;
@@ -59,7 +77,7 @@ class Query {
     const data = this.table.data;
     let parsedData: any[];
 
-    if (this._where)
+    if (this._where) {
       parsedData = data.json.filter((entry: any) => {
         let success = true;
         for (let whereItem of this._where) {
@@ -68,6 +86,9 @@ class Query {
         }
         return success;
       });
+    } else {
+      parsedData = data.json;
+    }
 
     if (this._limit) {
       let lPD: any[] = [];
@@ -85,12 +106,12 @@ class Query {
   public find(id: number = undefined): any {
     if (id) {
       const primary = this.table.keys.getPrimary();
-      console.log(primary);
+      return this.limit(1)
+        .where([[primary.name, id]])
+        .all()[0];
     }
 
-    return this.limit(1)
-      .where([["id", id]])
-      .all()[0];
+    return this.limit(1).all()[0];
   }
 
   public orderBy() {}
@@ -106,11 +127,171 @@ class Query {
   }
 }
 
+interface plugin_constructor_args {
+  arr: any[];
+  index: any;
+  key: string;
+}
+
+abstract class Plugin {
+  public name: string;
+
+  private static _plugins: string[];
+
+  constructor(
+    public arr: any,
+    public index: any,
+    public key: string,
+    public value: any
+  ) {}
+
+  abstract run(): { success: boolean; array: any };
+
+  public static get plugins() {
+    return Plugin._plugins;
+  }
+
+  public static registerPlugin(pluginName: string) {
+    //
+    this._plugins.push(pluginName);
+    //greeter.constructor.apply(greeter, new Array("World"));
+  }
+
+  //private static runPlugin(pluginName: )
+
+  public static updatePluginsMiddleware(
+    table: table,
+    entryKey: string,
+    ...args: any[]
+  ): boolean {
+    console.log("uPM entryKey", entryKey);
+    console.log("uPM ...args", ...args);
+    const entry = table.schema.getByName(entryKey);
+    const plugins = entry.plugins;
+
+    for (let plugin of plugins) {
+      let pluginInstance: Plugin;
+
+      try {
+        pluginInstance = DynamicClass.InstanceObject<Plugin>(
+          plugin_context,
+          plugin,
+          ...args
+        );
+        const { success, array } = pluginInstance.run();
+        console.log("run call ", plugin, array);
+        return success;
+      } catch (dcle: any) {
+        console.log("no plugin registred", plugin);
+        return false;
+      }
+    }
+
+    //plugin.apply(plugin, {arr,index,key});
+  }
+}
+
+export class AutoIncrement extends Plugin {
+  constructor(arr: any, index: any, key: string, value: any) {
+    super(arr, index, key, value);
+    this.name = this.toString();
+  }
+
+  run(): { success: boolean; array: any } {
+    this.arr[this.index][this.key] = this.value;
+
+    return { success: true, array: this.arr };
+  }
+}
+
+export class NotNull extends Plugin {
+  constructor(arr: any, index: any, key: string, value: any) {
+    super(arr, index, key, value);
+    this.name = this.toString();
+  }
+
+  run(): { success: boolean; array: any } {
+    if (this.value === null || this.value === undefined)
+      return { success: false, array: null };
+    return { success: true, array: null };
+  }
+}
+
+const plugin_context = {
+  AutoIncrement,
+  NotNull,
+};
+
 class QueryBuilder {
   constructor(private readonly table: table) {}
 
   Select(...selects: string[]): Query {
-    return new Query(this.table, "select");
+    return new Query(this.table, "select", selects);
+  }
+
+  Replace(...datas: any[]) {
+    let primary = this.table.keys.getPrimary();
+
+    for (let data of datas) {
+      if (data[primary.name]) {
+        let primKeyVal: string = data[primary.name].toString();
+        this.table.data.json.filter((entry: any, key: number) => {
+          if (entry[primary.name].toString() === primKeyVal) {
+            for (let newKey of this.table.schema.getIndexes()) {
+              //Object.keys(data)) {
+              if (newKey !== primary.name) {
+                if (
+                  !Plugin.updatePluginsMiddleware(
+                    this.table,
+                    newKey,
+                    "update",
+                    {
+                      arr: this.table.data.json,
+                      index: key,
+                      key: newKey,
+                      value: data[newKey],
+                    }
+                  )
+                ) {
+                  this.table.data.json[key][newKey] = data[newKey];
+                } else {
+                  console.log("data update abort");
+                }
+              }
+            }
+          }
+        });
+      } else {
+        //no primary
+        console.log("noprim");
+
+        let middlewareSuccess = true;
+        let dataArr = this.table.data.json;
+
+        for (let newKey of this.table.schema.getIndexes()) {
+          //dataArr.push(
+          if (
+            !Plugin.updatePluginsMiddleware(this.table, newKey, "insert", {
+              arr: dataArr,
+              index: dataArr.length - 1,
+              key: newKey,
+              value: dataArr.length,
+            })
+          ) {
+            middlewareSuccess = false;
+          }
+        }
+        if (!middlewareSuccess) {
+          console.log("data push abort");
+          return;
+        }
+        this.table.data.json = dataArr;
+
+        // let newID =
+        //   this.table.data.json[this.table.data.json.length - 1].id + 1;
+        // data.id = newID;
+      }
+    }
   }
 }
 
@@ -139,16 +320,12 @@ class JsonDBFile {
     const _table = new table(json);
     const qb = new QueryBuilder(_table);
 
-    console.log(
-      qb
-        .Select("id")
-        .where([
-          ["username", "gerredtor"],
-          ["password", "1234"],
-        ])
-        .limit(2)
-        .find(1)
+    qb.Replace(
+      { id: "1", username: "a", password: "b" },
+      { username: "a", password: "b" }
     );
+
+    console.log(qb.Select("id").all());
 
     return _table;
   }
